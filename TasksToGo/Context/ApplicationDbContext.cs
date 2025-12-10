622d0cb351631b93eb35fcd06273edba24bd133d
faiza-code <f2iza.na20@gmail.com>
2025-12-10 22:30:03 +0400

    Add project files.

---
+++ TasksToGo/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using TasksToGo.Models;
+
+namespace TasksToGo.Context
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<TodoTask> todoTasks { get; set; }
+        public DbSet<TaskCategory> TaskCategories { get; set; }
+
+    }
+}
